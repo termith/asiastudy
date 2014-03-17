136a2b0712265668656e27194a7ab6e67c8a88af
Anton Eryomin <antonerjomin@gmail.com>
2014-03-18 02:17:55 +0400

    version 0.0.4
    Features:
    
    Bugs:
    
    Others:
    add new variables for kana and symbols. delete 3 files: katakana, hiragana and symbols

--- flaskr/kanji_main.py
+++ flaskr/kanji_main.py
@@ -23,18 +23,46 @@ class Kanjies_text:
         self.value = self.value.replace("　", "").replace(" ", "")
 
     def remove_kana_symbols(self):
-        with open('core/katakana', 'r', encoding='utf-8') as fd:
-            katakana_list = fd.read().splitlines()
-        with open('core/hiragana', 'r', encoding='utf-8') as fd:
-            hiragana_list = fd.read().splitlines()
-        with open('core/symbols', 'r', encoding='utf-8') as fd:
-            symbols_list = fd.read().splitlines()
-        for items in katakana_list:
+        hira = [u'が', u'ぎ', u'ぐ', u'げ', u'ご', u'ざ', u'じ', u'ず', u'ぜ', u'ぞ', u'だ', u'ぢ', u'づ', u'で', u'ど',
+                u'ば', u'び', u'ぶ', u'べ', u'ぼ', u'ぱ', u'ぴ', u'ぷ', u'ぺ', u'ぽ', u'あ', u'い', u'う', u'え', u'お',
+                u'か', u'き', u'く', u'け', u'こ', u'さ', u'し', u'す', u'せ', u'そ', u'た', u'ち', u'つ', u'て', u'と',
+                u'な', u'に', u'ぬ', u'ね', u'の', u'は', u'ひ', u'ふ', u'へ', u'ほ', u'ま', u'み', u'む', u'め', u'も',
+                u'や', u'ゆ', u'よ', u'ら', u'り', u'る', u'れ', u'ろ', u'わ', u'を', u'ん', u'ぁ', u'ぃ', u'ぅ', u'ぇ',
+                u'ぉ', u'ゃ', u'ゅ', u'ょ', u'っ']
+        kata = [u'ガ', u'ギ', u'グ', u'ゲ', u'ゴ', u'ザ', u'ジ', u'ズ', u'ゼ', u'ゾ', u'ダ', u'ヂ', u'ヅ', u'デ', u'ド',
+                u'バ', u'ビ', u'ブ', u'ベ', u'ボ', u'パ', u'ピ', u'プ', u'ペ', u'ポ', u'ア', u'イ', u'ウ', u'エ', u'オ',
+                u'カ', u'キ', u'ク', u'ケ', u'コ', u'サ', u'シ', u'ス', u'セ', u'ソ', u'タ', u'チ', u'ツ', u'テ', u'ト',
+                u'ナ', u'ニ', u'ヌ', u'ネ', u'ノ', u'ハ', u'ヒ', u'フ', u'ヘ', u'ホ', u'マ', u'ミ', u'ム', u'メ', u'モ',
+                u'ヤ', u'ユ', u'ヨ', u'ラ', u'リ', u'ル', u'レ', u'ロ', u'ワ', u'ヲ', u'ン', u'ァ', u'ィ', u'ゥ', u'ェ',
+                u'ォ', u'ャ', u'ュ', u'ョ', u'ッ']
+        half = [u'ｶﾞ', u'ｷﾞ', u'ｸﾞ', u'ｹﾞ', u'ｺﾞ', u'ｻﾞ', u'ｼﾞ', u'ｽﾞ', u'ｾﾞ', u'ｿﾞ', u'ﾀﾞ', u'ﾁﾞ', u'ﾂﾞ', u'ﾃﾞ', u'ﾄﾞ',
+                u'ﾊﾞ', u'ﾋﾞ', u'ﾌﾞ', u'ﾍﾞ', u'ﾎﾞ', u'ﾊﾟ', u'ﾋﾟ', u'ﾌﾟ', u'ﾍﾟ', u'ﾎﾟ', u'ｱ', u'ｲ', u'ｳ', u'ｴ', u'ｵ', u'ｶ',
+                u'ｷ', u'ｸ', u'ｹ', u'ｺ', u'ｻ', u'ｼ', u'ｽ', u'ｾ', u'ｿ', u'ﾀ', u'ﾁ', u'ﾂ', u'ﾃ', u'ﾄ', u'ﾅ', u'ﾆ', u'ﾇ',
+                u'ﾈ', u'ﾉ', u'ﾊ', u'ﾋ', u'ﾌ', u'ﾍ', u'ﾎ', u'ﾏ', u'ﾐ', u'ﾑ', u'ﾒ', u'ﾓ', u'ﾔ', u'ﾕ', u'ﾖ', u'ﾗ', u'ﾘ',
+                u'ﾙ', u'ﾚ', u'ﾛ', u'ﾜ', u'ｦ', u'ﾝ', u'ｧ', u'ｨ', u'ｩ', u'ｪ', u'ｫ', u'ｬ', u'ｭ', u'ｮ', u'ｯ']
+        kana = hira + kata + half
+
+        wnum = [u'０', u'１', u'２', u'３', u'４', u'５', u'６', u'７', u'８', u'９']
+        hnum = [u'0', u'1', u'2', u'3', u'4', u'5', u'6', u'7', u'8', u'9']
+        walp = [u'ａ', u'ｂ', u'ｃ', u'ｄ', u'ｅ', u'ｆ', u'ｇ', u'ｈ', u'ｉ', u'ｊ', u'ｋ', u'ｌ', u'ｍ', u'ｎ', u'ｏ',
+                u'ｐ', u'ｑ', u'ｒ', u'ｓ', u'ｔ', u'ｕ', u'ｖ', u'ｗ', u'ｘ', u'ｙ', u'ｚ', u'Ａ', u'Ｂ', u'Ｃ', u'Ｄ',
+                u'Ｅ', u'Ｆ', u'Ｇ', u'Ｈ', u'Ｉ', u'Ｊ', u'Ｋ', u'Ｌ', u'Ｍ', u'Ｎ', u'Ｏ', u'Ｐ', u'Ｑ', u'Ｒ', u'Ｓ',
+                u'Ｔ', u'Ｕ', u'Ｖ', u'Ｗ', u'Ｘ', u'Ｙ', u'Ｚ']
+        halp = [u'a', u'b', u'c', u'd', u'e', u'f', u'g', u'h', u'i', u'j', u'k', u'l', u'm', u'n', u'o', u'p', u'q',
+                u'r', u's', u't', u'u', u'v', u'w', u'x', u'y', u'z', u'A', u'B', u'C', u'D', u'E', u'F', u'G', u'H',
+                u'I', u'J', u'K', u'L', u'M', u'N', u'O', u'P', u'Q', u'R', u'S', u'T', u'U', u'V', u'W', u'X', u'Y',
+                u'Z']
+        wsym = [u'！', u'”', u'＃', u'＄', u'％', u'＆', u'’', u'（', u'）', u'＊', u'＋', u'、', u'−', u'．', u'／',
+                u'：', u'；', u'＜', u'＝', u'＞', u'？', u'＠', u'「', u'＼', u'」', u'＾', u'＿', u'｀', u'『', u'｜',
+                u'』', u'〜', u'、', u'。']
+        hsym = [u'!', u'\"', u'#', u'$', u'%', u'&', u'\'', u'(', u')', u'*', u'+', u',', u'-', u'.', u'/', u':', u';',
+                u'<', u'=', u'>', u'?', u'@', u'[', u'\\', u']', u'^', u'_', u'`', u'{', u'|', u'}', u'~']
+        symbols = wnum + hnum + walp + halp + wsym + hsym
+
+        for items in kana:
             self.value = self.value.replace(items, "")
-            for symb in hiragana_list:
+            for symb in symbols:
                 self.value = self.value.replace(symb, "")
-                for symb in symbols_list:
-                    self.value = self.value.replace(symb, "")
 
     def take_percent_count(self):
         total = len(self.value)
