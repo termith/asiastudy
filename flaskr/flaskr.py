0ac612ffb665f7ee4a5d08a4a4e84fb18e370702
OdinO4ka <antonerjomin@gmail.com>
2014-03-24 15:00:27 +0400

    version 0.0.6
    Features:
    
    Bugs:
    
    Others:
    delete working with BD

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -12,117 +12,28 @@ from kanji_main import KanjiesText
 # create our little application
 app = Flask(__name__)
 
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-    DEBUG=True,
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
-
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-
-def init_db():
-    """Creates the database tables."""
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-
-
-def query_db(query, args=(), one=False):
-    """Method for DB connect."""
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
-
+analyzed_text = 'Please enter your text and then press key.'
+list_of_percents = []
 
 @app.route('/')
 def show_entries():
-    db = get_db()
-    cur = db.execute('select * from entries where id=(select max(id) from entries)')
-    entries = cur.fetchall()
-    return render_template('show_entries.html', entries=entries)
+    global analyzed_text
+    global list_of_percents
+    return render_template('show_entries.html', analyzed_text = analyzed_text, list_of_percents = list_of_percents)
 
 
 @app.route('/add', methods=['POST'])
 def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    db.execute('insert into entries (text) values (?)', [request.form['text']])
-    db.commit()
-    flash('New entry was successfully posted')
-
-    a = KanjiesText(show_part_db('text'))
+    global analyzed_text
+    global list_of_percents
+    analyzed_text = request.form['text']
+    a = KanjiesText(analyzed_text)
     a.remove_spaces_from_text()
     a.remove_kana_symbols()
-    db.execute('update entries set clear_text = (?) where id=(select max(id) from entries)', [a.value])
-    db.commit()
-    b = a.take_percent_count()
-    for i in range(5):
-        level = "N{0}".format(i+1)
-        db.execute('update entries set {} = (?) where id=(select max(id) from entries)'.format(level), [b[i]])
-        db.commit()
-    return redirect(url_for('show_entries'))
-
-
-def show_part_db(part):
-    for t in query_db('select * from entries where id=(select max(id) from entries)'):
-        return t[part]
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
-
-@app.route('/home', methods=['GET'])
-
-
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
+    list_of_percents = a.take_percent_count()
+    print(list_of_percents)
     return redirect(url_for('show_entries'))
 
 
 if __name__ == '__main__':
-    init_db()
     app.run()
